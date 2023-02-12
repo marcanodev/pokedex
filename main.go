cddf7730e77b9c4bccf3abbefc419af185e63388
marcanodev <106038850+marcanodev@users.noreply.github.com>
2023-02-12 11:22:08 -0500

    Add interactive prompt boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	progName = "Pokedex"
+)
+
+var input string
+var scanner *bufio.Scanner
+var prompt = fmt.Sprintf("%s > ", strings.ToLower(progName))
 
 func main() {
-	fmt.Println("Hello World")
+	scanner = bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		scanner.Scan()
+		input = scanner.Text()
+
+		switch input {
+		case "q":
+			fmt.Println("Come back again soon!")
+			os.Exit(0)
+		case "h":
+			fmt.Println("help message")
+		}
+	}
 }
