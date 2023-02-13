273917c6cd56e80f064bf3bc9caeb3d5d28a7bf9
marcanodev <106038850+marcanodev@users.noreply.github.com>
2023-02-12 22:06:36 -0500

    pokedex/pokeapi: Reset to commit 18c4f6c
    I decided to reset head to commit 18c4f6c due to annoying
    go mod import errors. All code has been simply copied and pasted
    from commit 684ddf16a727d8283744d9a265f15e7a6d0d7e04
    and the program is running as expected. Most importantly, the
    go mod errors are gone.
    
    Note: The pokedex binary has also been rebuilt in this current state.

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,1 +1,37 @@
 package pokeapi
+
+import (
+	"errors"
+	"io"
+	"net/http"
+)
+
+type Locations struct {
+	Count   int         `json:"count"`
+	Next    *string     `json:"next"`
+	Prev    interface{} `json:"previous"`
+	Results []struct {
+		Name *string `json:"name"`
+		Url  *string `json:"url"`
+	} `json:"results"`
+}
+
+func GetJSON(url string) (JSON []byte, e error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		e = err
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if resp.StatusCode > 299 {
+		err = errors.New("response failed")
+	}
+
+	if err != nil {
+		e = err
+		return
+	}
+	return body, e
+}
--- main.go
+++ main.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	api "github.com/marcanodev/pokedex/internal/pokeapi"
 )
 
 const (
 	progName = "Pokedex"
 	helpDesc = "Displays a help message"
 	exitDesc = "Exit the pokedex"
+	URL      = "https://pokeapi.co/api/v2/location/"
 )
 
 type cmd struct {
 	name        string
 	description string
 	callback    func() error
+	mapLoc
+}
+
+type mapLoc struct {
+	Next string
+	Prev interface{}
 }
 
 var validCmd = make(map[string]cmd)
+
+var mapNextCmd cmd
+var mapPrevCmd cmd
 
 var input string
 var scanner *bufio.Scanner
@@ -38,6 +52,24 @@ func main() {
 			os.Exit(0)
 		case "help":
 			fmt.Fprint(os.Stdout, usageMsg)
+		case "map":
+			locs := &api.Locations{}
+			data, err := api.GetJSON(URL)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = json.Unmarshal(data, locs)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			mapNextCmd.Next = *locs.Next
+			mapPrevCmd.Prev = locs.Prev
+			for _, l := range locs.Results {
+				fmt.Fprintln(os.Stdout, string(*l.Name))
+			}
 		default:
 			fmt.Fprint(os.Stdout, usageMsg)
 		}
@@ -46,17 +78,12 @@ func main() {
 }
 
 func init() {
-	validCmd["help"] = cmd{
-		name:        "help",
-		description: helpDesc,
-		callback:    helpCmd,
-	}
-
-	validCmd["exit"] = cmd{
-		name:        "exit",
-		description: exitDesc,
-		callback:    exitCmd,
-	}
+	help := addCmd("help", helpDesc, helpCB)
+	exit := addCmd("exit", exitDesc, exitCB)
+	mapNextCmd = addCmd("map",
+		"Shows next 20 locations areas of the Pokemon world", mapCB)
+	mapPrevCmd = addCmd("mapb",
+		"Shows the prev 20 locations of the Pokemon world", mapCB)
 
 	usageMsg = fmt.Sprintf(`Welcome to the %s!
 Usage:
@@ -65,14 +92,29 @@ Usage:
 %s: %s
 
 `, progName,
-		validCmd["help"].name, validCmd["help"].description,
-		validCmd["exit"].name, validCmd["exit"].description)
+		help.name, help.description,
+		exit.name, exit.description)
 }
 
-func helpCmd() error {
+func addCmd(name string, desc string, cb func() error) cmd {
+	name = strings.ToLower(name)
+	validCmd[name] = cmd{
+		name:        name,
+		description: desc,
+		callback:    cb,
+	}
+
+	return validCmd[name]
+}
+
+func helpCB() error {
 	return nil
 }
 
-func exitCmd() error {
+func exitCB() error {
+	return nil
+}
+
+func mapCB() error {
 	return nil
 }
